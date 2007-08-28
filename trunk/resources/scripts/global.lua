6efa580f642232bd6072698fac953d34f07f4e11
daviddq <daviddq@3342a6a6-5e2d-0410-95e1-bf50fdd259a6>
2007-08-28 18:04:14 +0000

    - Nuevos recursos.

---
+++ trunk/resources/scripts/global.lua
@@ -0,0 +1,12 @@
+
+Log:Instance():LuaWrite("***********************************");
+Log:Instance():LuaWrite("*** Iniciando sistema de script ***");
+Log:Instance():LuaWrite("***********************************");
+
+Enemy_OnWayPoint = function()
+	Log:Instance():LuaWrite("El enemigo ha llegado a un WayPoint");
+end
+
+Heroe_OnJump = function()
+	Log:Instance():LuaWrite("El héroe ha saltado");
+end
