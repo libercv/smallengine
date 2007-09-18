e31e5b95809a74d31e66620635e4426b0b5bcff3
daviddq <daviddq@3342a6a6-5e2d-0410-95e1-bf50fdd259a6>
2007-09-18 06:15:45 +0000

    - Nuevo script que controla colisiones con items y enemigos.

--- trunk/resources/scripts/global.lua
+++ trunk/resources/scripts/global.lua
@@ -3,15 +3,32 @@ Log:Instance():LuaWrite("***********************************");
 Log:Instance():LuaWrite("*** Iniciando sistema de script ***");
 Log:Instance():LuaWrite("***********************************");
 
-Enemy_OnWayPoint = function()
+Enemy_OnWayPoint = function(id)
 	Log:Instance():LuaWrite("El enemigo ha llegado a un WayPoint");
 end
 
-Heroe_OnJump = function()
+Heroe_OnJump = function(id)
 	Log:Instance():LuaWrite("El héroe ha saltado");
 end
 
-Heroe_OnCollision = function()
-	Log:Instance():LuaWrite("El héroe ha chocado con un enemigo");
-	StateGame:Instance():KillPlayer();
+player1_OnCollision = function(id)
+	Heroe_OnCollision("player1", id)
+end
+
+player2_OnCollision = function(id)
+	Heroe_OnCollision("player2", id)
+end
+
+Heroe_OnCollision = function(player, id)
+	Log:Instance():LuaWrite("El héroe ha chocado con "..id);
+
+	if string.find(id, "Yoshi") ~= nil then
+		StateGame:Instance():KillPlayer(player);
+	elseif id == "trigger1" then
+		StateGame:Instance():SetActiveCamera(0);
+	elseif id == "trigger2" then
+		StateGame:Instance():SetActiveCamera(1);
+	elseif string.find(id, "bola") ~= nil then
+		StateGame:Instance():KillObject(player, id);
+	end
 end
